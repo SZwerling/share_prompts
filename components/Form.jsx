428c2d6c790bdd97e563288ef614790d705b7a11
unknown <sazwerling@gmail.com>
2023-08-21 20:59:57 -0700

    prompt route connected and prompt db

--- components/Form.jsx
+++ components/Form.jsx
@@ -32,7 +32,7 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
                </span>
                <input
                   value={post.tag}
-                  onChange={(e) => setPost({ ...post, tag })}
+                  onChange={(e) => setPost({ ...post, tag: e.target.value })}
                   placeholder="#tag"
                   required
                   className="form_input"
